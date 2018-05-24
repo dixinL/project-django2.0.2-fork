f244337a56e2dd7552a90aca48403b0adccbfb1a
dixinl <dixinl@163.com>
2018-05-24 09:13:38 +0800

    w

--- problem/delete.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import redirect
-from .models import Problem
-
-
-def problem_delete(request, id):
-    problem = Problem.objects.get(id = id)
-    series = problem.series
-    problem.delete()
-    return redirect('problem_list',series)
